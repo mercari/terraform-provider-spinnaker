ab783fd412c86e760712df818692e329aa27f8f0
KeisukeYamashita <19yamashita15@gmail.com>
2020-07-16 19:51:24 +0900

    tpm

--- spinnaker/api/application_test.go
+++ spinnaker/api/application_test.go
@@ -12,7 +12,7 @@ func TestValidateApplicationCloudProviders(t *testing.T) {
 		shouldPass     bool
 	}{
 		"pass":                                          {"kubernetes", "test-1", true},
-		"pass with multiple provider":                   {"kubernetes,gce", "test1", true},
+		"pass with multiple provider":                   {"kubernetes,gce,", "test1", true},
 		"fail with single invalid cloudProvider":        {"mercari", "test-1", false},
 		"fail with valid and invalid mix cloudProvider": {"kubernetes,mercari", "test-1", false},
 		"fail with over max lenght":                     {"kubernetes", "very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-long-app", false},
--- spinnaker/data_source_spinnaker_application_test.go
+++ spinnaker/data_source_spinnaker_application_test.go
@@ -1,7 +1,10 @@
 package spinnaker
 
 import (
+	"math/rand"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
@@ -21,7 +24,52 @@ func TestAccDataSourceSpinnakerApplication_basic(t *testing.T) {
 					testAccCheckSpinnakerApplicationExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "name", rName),
 					resource.TestCheckResourceAttr(resourceName, "email", "acceptance@test.com"),
-					resource.TestCheckResourceAttr(resourceName, "instance_port", "80"),
+					resource.TestCheckResourceAttr(resourceName, "instance_port", strconv.Itoa(defaultInstancePort)),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceSpinnakerApplication_instancePort(t *testing.T) {
+	resourceName := "spinnaker_application.test"
+	rand.Seed(time.Now().UnixNano())
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	rPort := rand.Intn(8000) + 1 // avoid 0
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSpinnakerApplicatioDestroy(resourceName),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSpinnakerApplication_instancePort(rName, rPort),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSpinnakerApplicationExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "email", "acceptance@test.com"),
+					resource.TestCheckResourceAttr(resourceName, "instance_port", strconv.Itoa(rPort)),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceSpinnakerApplication_cloudProviders(t *testing.T) {
+	resourceName := "spinnaker_application.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	cloudProvider := "kubernetes"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSpinnakerApplicatioDestroy(resourceName),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSpinnakerApplication_cloudProvider(rName, cloudProvider),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSpinnakerApplicationExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "email", "acceptance@test.com"),
+					resource.TestCheckResourceAttr(resourceName, "instance_port", strconv.Itoa(defaultInstancePort)),
 				),
 			},
 		},
