0d67b9f1f2d3ae286745df3bb0e7003976556c0b
KeisukeYamashita <19yamashita15@gmail.com>
2020-07-17 11:13:19 +0900

    fix(spinnaker): testcase for validation application

--- spinnaker/api/application_test.go
+++ spinnaker/api/application_test.go
@@ -1,26 +1,25 @@
 package api
 
 import (
-	"strings"
 	"testing"
 )
 
 func TestValidateApplicationCloudProviders(t *testing.T) {
 	tcs := map[string]struct {
-		cloudProviders string
+		cloudProviders []string
 		appName        string
 		shouldPass     bool
 	}{
-		"pass":                                          {"kubernetes", "test-1", true},
-		"pass with multiple provider":                   {"kubernetes,gce,", "test1", true},
-		"fail with single invalid cloudProvider":        {"mercari", "test-1", false},
-		"fail with valid and invalid mix cloudProvider": {"kubernetes,mercari", "test-1", false},
-		"fail with over max lenght":                     {"kubernetes", "very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-long-app", false},
+		"pass":                                          {[]string{"kubernetes"}, "test-1", true},
+		"pass with multiple provider":                   {[]string{"kubernetes", "gce"}, "test1", true},
+		"fail with single invalid cloudProvider":        {[]string{"mercari"}, "test-1", false},
+		"fail with valid and invalid mix cloudProvider": {[]string{"kubernetes","mercari"}, "test-1", false},
+		"fail with over max lenght":                     {[]string{"kubernetes"}, "very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-long-app", false},
 	}
 
 	for n, tc := range tcs {
 		t.Run(n, func(t *testing.T) {
-			for _, p := range strings.Split(tc.cloudProviders, ",") {
+			for _, p := range tc.cloudProviders {
 				err := validateSpinnakerApplicationNameByCloudProvider(tc.appName, p)
 				if err != nil && tc.shouldPass {
 					t.Fatalf("failed: %v", err)
