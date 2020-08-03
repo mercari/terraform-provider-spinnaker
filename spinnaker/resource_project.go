9b385767a98eeac160df232a06dc1c11f813de59
KeisukeYamashita <19yamashita15@gmail.com>
2020-08-03 16:15:18 +0900

    chore(spinnaker): introduce terraform plugin sdk

--- spinnaker/resource_project.go
+++ spinnaker/resource_project.go
@@ -3,7 +3,7 @@ package spinnaker
 import (
 	"strings"
 
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/mercari/terraform-provider-spinnaker/spinnaker/api"
 )
 
